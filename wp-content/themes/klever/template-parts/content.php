7cdb33d2a8a4e9d4805b40136dea53e2cf2648c3
Ladlen <TwilightTower@mail.ru>
2017-08-09 19:51:09 +0300

    css; template changes

--- wp-content/themes/klever/template-parts/content.php
+++ wp-content/themes/klever/template-parts/content.php
@@ -28,6 +28,27 @@
             </div>
         </div>
 
+        <a class="moretag" href="<?php echo get_permalink(get_post()->ID) ?>"> Read the full article...</a>
+
+        <div class="entry-content">
+            <?php
+            /* translators: %s: Name of current post */
+            the_content( sprintf(
+                __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+                get_the_title()
+            ) );
+
+            wp_link_pages( array(
+                'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+                'after'       => '</div>',
+                'link_before' => '<span>',
+                'link_after'  => '</span>',
+                'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+                'separator'   => '<span class="screen-reader-text">, </span>',
+            ) );
+            ?>
+        </div><!-- .entry-content -->
+
         <footer class="entry-footer">
             <?php twentysixteen_entry_meta(); ?>
 		<?php
